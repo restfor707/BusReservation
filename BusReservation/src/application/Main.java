b5ab243a8b714ef5d516dc5bcfcfc0183d869bb0
restfor707 <restfor707@gmail.com>
2023-04-17 16:12:08 +0900

    Fix Main.java & result.fxml

--- BusReservation/src/application/Main.java
+++ BusReservation/src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,10 +12,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("result.fxml"));
+			// BorderPane root = new BorderPane();
+			// Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
+			
+			primaryStage.setTitle("AppMain");
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
